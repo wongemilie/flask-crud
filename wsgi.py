71a12050c502e0bbfed7ad5decbae91bd2d1cab9
EmilieWong <wongemilie@gmail.com>
2020-12-16 12:45:40 +0100

    Adding /api/v1/products endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,25 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World ! :)"
+
+
+@app.route('/api/v1/products')
+def products():
+
+    products = {
+        1: { 'id': 1, 'name': 'Skello' },
+        2: { 'id': 2, 'name': 'Socialive.tv' },
+    }
+
+    # products = {
+    #     1: 'Skello',
+    #     2: 'Socialive.tv',
+    # }
+
+    return jsonify(products)
